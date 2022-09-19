75ab5c06a89710e17a24f679bbd96195e8ebaf8c
nicolaswiedel <nicolaswiedel@googlemail.com>
2022-09-19 18:04:31 +0200

    ProductScreen Part 1

--- ProNatur GmbH/MainMenuScreen.Designer.cs
+++ ProNatur GmbH/MainMenuScreen.Designer.cs
@@ -29,13 +29,44 @@ namespace ProNatur_GmbH
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainMenuScreen));
+            this.btnProducts = new System.Windows.Forms.Button();
+            this.btnBill = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // btnProducts
+            // 
+            this.btnProducts.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnProducts.BackgroundImage")));
+            this.btnProducts.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnProducts.ForeColor = System.Drawing.Color.White;
+            this.btnProducts.Location = new System.Drawing.Point(12, 36);
+            this.btnProducts.Name = "btnProducts";
+            this.btnProducts.Size = new System.Drawing.Size(275, 125);
+            this.btnProducts.TabIndex = 0;
+            this.btnProducts.Text = "Produkte verwalten";
+            this.btnProducts.UseVisualStyleBackColor = true;
+            this.btnProducts.Click += new System.EventHandler(this.btnProducts_Click);
+            // 
+            // btnBill
+            // 
+            this.btnBill.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnBill.BackgroundImage")));
+            this.btnBill.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnBill.ForeColor = System.Drawing.Color.White;
+            this.btnBill.Location = new System.Drawing.Point(336, 36);
+            this.btnBill.Name = "btnBill";
+            this.btnBill.Size = new System.Drawing.Size(275, 125);
+            this.btnBill.TabIndex = 1;
+            this.btnBill.Text = "Rechnung erstellen";
+            this.btnBill.UseVisualStyleBackColor = true;
             // 
             // MainMenuScreen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.ClientSize = new System.Drawing.Size(623, 212);
+            this.Controls.Add(this.btnBill);
+            this.Controls.Add(this.btnProducts);
             this.Name = "MainMenuScreen";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Hauptmenü";
@@ -44,5 +75,8 @@ namespace ProNatur_GmbH
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnProducts;
+        private System.Windows.Forms.Button btnBill;
     }
 }
--- ProNatur GmbH/MainMenuScreen.cs
+++ ProNatur GmbH/MainMenuScreen.cs
@@ -14,5 +14,12 @@ namespace ProNatur_GmbH
         {
             InitializeComponent();
         }
+
+        private void btnProducts_Click(object sender, EventArgs e)
+        {
+            ProductScreen productScreen = new ProductScreen();
+            productScreen.Show();
+            this.Hide();
+        }
     }
 }
